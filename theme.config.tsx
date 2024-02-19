69acf1d25f9b03cfcc10900258297f50662433e2
Maanas Verma <maanas.verma@icicibank.com>
2024-02-20 03:53:55 +0530

    Remove advanced and satori pages, add @svgr/webpack as a dev dependency, update _meta.json, update about page, update tsconfig.json, update theme.config.tsx, update index page, update next.config.js

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image';
+import {
+  snapkitsLogo
+} from '@components/icons'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: snapkitsLogo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/snapkits/snapkits.github.io',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MIT 2024 © Snapkits',
   },
 }
 
